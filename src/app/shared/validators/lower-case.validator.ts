f643c5576df11fff238f9a826bda791ede2ae4e6
alini <alini.canedo@gmail.com>
2020-12-19 12:02:37 -0300

    registrando um novo usuario

---
+++ src/app/shared/validators/lower-case.validator.ts
@@ -0,0 +1,9 @@
+import { IfStmt } from "@angular/compiler";
+import { AbstractControl } from "@angular/forms";
+
+export function lowerCaseValidator(control: AbstractControl) {
+    if(control.value.trim() && !/^[a-z0-9_\-]+$/.test(control.value)) {
+        return {lowerCase: true}
+    }
+        return null;
+}
