519d2cafee8439a59da273d1b903c61f2f07ce42
alini <alini.canedo@gmail.com>
2021-01-17 11:44:58 -0300

    Remoção de fotos e novos componentes

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -8,9 +8,24 @@ import { RouterModule } from "@angular/router";
 import { ReactiveFormsModule } from "@angular/forms";
 import { VMessageModule } from "src/app/shared/components/vmessage/vmessage.module";
 import { PhotoCommentsComponent } from "./photo-comments/photo-comments.component";
+import { PhotoOwnerOnlyDirective } from "./photo-owner-only/photo-owner-only.directive";
 @NgModule({
-    declarations: [PhotoDetailsComponent, PhotoCommentsComponent],
-    exports: [PhotoDetailsComponent, PhotoCommentsComponent],
-    imports: [  CommonModule, PhotoModule, RouterModule, ReactiveFormsModule, VMessageModule]
+    declarations: [
+                   PhotoDetailsComponent,
+                   PhotoCommentsComponent,
+                   PhotoOwnerOnlyDirective
+                 ],
+    exports: [
+              PhotoDetailsComponent, 
+              PhotoCommentsComponent
+            ],
+
+    imports: [  
+                CommonModule, 
+                PhotoModule, 
+                RouterModule, 
+                ReactiveFormsModule, 
+                VMessageModule
+            ]
 })
 export class PhotoDetailsModule { }
